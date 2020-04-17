3f613850ef0ee4a96e1a20ed6df5b9fd55581a00
Pawel <psob96@gmail.com>
2020-04-17 23:28:43 +0200

    ID zmienione na autogenerate

--- app/src/main/java/com/pollub/samoloty/database/Plane.kt
+++ app/src/main/java/com/pollub/samoloty/database/Plane.kt
@@ -3,13 +3,14 @@ package com.pollub.samoloty.database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-        @Entity
-        data class Plane(
+@Entity
+data class Plane(
 
-          @PrimaryKey
-          val id: Long,
-          val modelFilename: String,
-          val textureFilename: String,
-          val targetName: String,
-          val productionYear: Int
-        )
+        val modelFilename: String,
+        val textureFilename: String,
+        val targetName: String,
+        val productionYear: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+}
