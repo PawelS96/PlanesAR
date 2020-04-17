18b5df5818a9ac8902acb4efdeff2cc745a0bd5f
Pawel <psob96@gmail.com>
2020-04-17 23:30:03 +0200

    modele gotowe do testów

--- app/src/main/java/com/pollub/samoloty/database/DatabaseAccess.kt
+++ app/src/main/java/com/pollub/samoloty/database/DatabaseAccess.kt
@@ -12,13 +12,28 @@ abstract class DatabaseAccess : RoomDatabase() {
 
     fun populate() {
 
-        if (planeDAO().count() == 0) {
+        planeDAO().clear()
 
-            planeDAO().insert(
-                    Plane(1, "plane3.obj", "planeTexture.png", "p-39", 1),
-                    Plane(2, "plane3.obj", "planeTexture.png", "F4U1", 2)
-            )
-        }
+        planeDAO().insert(
+
+                //model OK tekstura OK
+                Plane("p39.obj", "p39.png", "P39", 1940)
+
+                //do sprawdzenia
+                //jak się nie wyświetla albo telepie można kombinowac z size w PlanesDatabase.xml
+                //większy size daje mniejszy model i mniej telepania
+           /*
+           Plane("arsenal.obj", "p39.png", "Arsenal_VG_33", 1939),
+                Plane("b17.obj", "p39.png", "b17", 1939),
+                Plane("t18.obj", "p39.png", "t18", 1919),
+                Plane("dorand.obj", "dorand.jpg", "Dorand_AR1", 1917),
+                Plane("ju87.obj", "p39.png", "ju87", 1937),
+                Plane("t6.obj", "p39.png", "North_American_T-6_Texan", 1935),
+                Plane("spitfire.obj", "spitfire.png", "Spitfire", 1938),
+                Plane("camel.obj", "p39.png", "f1camel", 1917),
+                Plane("thunderbolt.obj", "p39.png", "Republic_P-47_Thunderbolt", 1941)
+                */
+                )
     }
 
     companion object {
